f972a7c180d24064cb15d1884dfaf3911fac694a
LAPTOP-GDVL414K\wyadmins <yong.wang@ronds.com.cn>
2020-09-17 11:09:22 +0800

    [update]:alg011增加运行、停机时间指标

--- alg011_MG2250.py
+++ alg011_MG2250.py
@@ -1,17 +1,19 @@
 """
 Provides:
-电机启机次数统计
+启机次数统计
 ==============
 Input Signals (1):
 * 启停标志位（0-1序列）：drive_run
 
 Parameter Configs (1)：
-* 启机标志位
+* 启机标志位 [0, 1]
 ==============
 Outputs:
 指标   |  指标id
 ---------------------
-* 启机次数：  11107
+* 启机次数：  11100
+* 运行时长：  11101
+* 停机时长：  11102
 """
 
 import numpy as np
@@ -25,21 +27,36 @@ class Alg011:
     @staticmethod
     def get_fe(df, algparas):
         measdate = []
-        switch_nums = []
+        switch_nums = 0
+        run_time = 0
+        stop_time = 0
         if not df.empty:
-            if algparas == 0:
-                n = sum(np.diff(df.drive_run) == -1)  # 启机状态为0
+            measdate = df.index[0]
+            if algparas == 0:  # 启机状态标志为0
+                n = sum(-1 == np.diff(df.drive_run))   # 启机次数
+                n_run = sum(df.drive_run == 0)
             else:
-                n = sum(np.diff(df.drive_run) == 1)
+                n = sum(1 == np.diff(df.drive_run))
+                n_run = sum(df.drive_run == 1)
+            run_time = n_run * df.dt  # 运行时长
+            stop_time = (df.shape[0] - n_run) * df.dt  # 停机时长
             if n > 0:
-                switch_nums.append(n)
-                measdate.append(df.index[0])
-        return measdate, switch_nums
+                switch_nums = n
+        return measdate, switch_nums, run_time, stop_time
 
     def execute(self):
         df = self.graph.get_data_from_api(['drive_run'])
-        measdate, switch_nums = self.get_fe(df, self.graph.parameter)
-        for t, v in zip(measdate, switch_nums):
-            index = Index({'assetid': self.graph.deviceid, 'meastime1st': t, 'feid1st': "11107",
-                           'value1st': float(v), 'indices2nd': []})
-            self.graph.indices.append(index)
+        measdate, switch_nums, run_time, stop_time = self.get_fe(df, self.graph.parameter)
+        if measdate:
+            if switch_nums > 0:
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11100",
+                               'value1st': float(switch_nums), 'indices2nd': []})
+                self.graph.indices.append(index)
+            if run_time > 0:
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11101",
+                               'value1st': float(run_time), 'indices2nd': []})
+                self.graph.indices.append(index)
+            if stop_time > 0:
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11102",
+                               'value1st': float(stop_time), 'indices2nd': []})
+                self.graph.indices.append(index)
