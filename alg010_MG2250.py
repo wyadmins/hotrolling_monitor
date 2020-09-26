019776297152ee6d26e53f7236885c8817d9eb79
LAPTOP-GDVL414K\wyadmins <yong.wang@ronds.com.cn>
2020-09-26 14:59:03 +0800

    [update]:电机里程策略更新，没有压力控制点位情况下，只计算总里程

--- alg010_MG2250.py
+++ alg010_MG2250.py
@@ -4,7 +4,7 @@ Provides:
 ==============
 Input Signals (2):
 * 实际转速：speed
-* 压力控制模式：in_force_control
+* 压力控制模式：in_force_control 【可选】
 
 Parameter Configs (0)：
 ==============
@@ -12,8 +12,8 @@ Outputs:
 指标   |  指标id
 ---------------------
 * 电机运行总里程：11000
-* 电机负载总里程：10001
-* 电机空载总里程：10002
+* 电机负载总里程：10001 【可选】
+* 电机空载总里程：10002 【可选】
 """
 
 import numpy as np
@@ -27,14 +27,30 @@ class Alg010:
         self.graph = graph
 
     @staticmethod
-    def get_fe(df):
+    def get_fe_v1(df):
+        """
+        计算总负载里程
+        """
+        measdate = []
+        s_total = 0
+        if not df.empty:
+            measdate = df.index[0]   # 时间
+            s_total = scipy.trapz(np.abs(df.speed)) * df.dt  # 总里程
+
+        return measdate, s_total
+
+    @staticmethod
+    def get_fe_v2(df):
+        """
+        通过压力模式计算负载、空载里程指标
+        """
         measdate = []
         s_total = 0
         s_in_operation = 0
         if not df.empty:
             measdate = df.index[0]   # 时间
             idx = (df.in_force_control == 1)
-            re_iter = com_util.Reg.finditer(idx, 0.5*df.num_per_sec)
+            re_iter = com_util.Reg.finditer(idx, 0.5*df.num_per_sec)   # 负载时间大于0.5秒计入
             s_total = scipy.trapz(np.abs(df.speed)) * df.dt  # 总里程
             for i in re_iter:
                 [stidx, edidx] = i.span()
@@ -43,17 +59,25 @@ class Alg010:
         return measdate, s_total, s_in_operation, max(0, s_total-s_in_operation)
 
     def execute(self):
-        df = self.graph.get_data_from_api(['speed', 'in_force_control'])
-        measdate, s_total, s_in_operation, s_idler = self.get_fe(df)
-        if measdate:
-            index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11000",
-                           'value1st': s_total, 'indices2nd': []})
-            self.graph.indices.append(index)
+        if 1 == len(self.graph.channelid):
+            df = self.graph.get_data_from_api(['speed'])
+            measdate, s_total = self.get_fe_v1(df)   # 计算总里程
+            if measdate:
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11000",
+                               'value1st': s_total, 'indices2nd': []})
+                self.graph.indices.append(index)
+        else:
+            df = self.graph.get_data_from_api(['speed', 'in_force_control'])
+            measdate, s_total, s_in_operation, s_idler = self.get_fe_v2(df)  # 计算总里程、负载里程、空载里程
+            if measdate:
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11000",
+                               'value1st': s_total, 'indices2nd': []})
+                self.graph.indices.append(index)
 
-            index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11001",
-                           'value1st': s_in_operation, 'indices2nd': []})
-            self.graph.indices.append(index)
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11001",
+                               'value1st': s_in_operation, 'indices2nd': []})
+                self.graph.indices.append(index)
 
-            index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11002",
-                           'value1st': s_idler, 'indices2nd': []})
-            self.graph.indices.append(index)
+                index = Index({'assetid': self.graph.deviceid, 'meastime1st': measdate, 'feid1st': "11002",
+                               'value1st': s_idler, 'indices2nd': []})
+                self.graph.indices.append(index)
