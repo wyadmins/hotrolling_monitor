c1b14e1c5a5719442da3bbc51184fb94cb58a7fe
LAPTOP-GDVL414K\wyadmins <yong.wang@ronds.com.cn>
2020-11-18 20:52:42 +0800

    [update]:更新注册算法

--- __init__baowu.py
+++ __init__baowu.py
@@ -8,6 +8,12 @@ from alg001_s2_MG2250 import Alg001_S2
 from alg003_MG2250 import Alg003
 from alg004_MG2250 import Alg004
 from alg004_s2_MG2250 import Alg004_S2
+from alg004_s3_MG2250 import Alg004_S3
+from alg004_s4_MG2250 import Alg004_S4
+from alg004_s5_MG2250 import Alg004_S5
+from alg004_s6_MG2250 import Alg004_S6
+from alg004_s7_MG2250 import Alg004_S7
+from alg004_s8_MG2250 import Alg004_S8
 from alg005_MG2250 import Alg005
 from alg010_MG2250 import Alg010
 from alg011_MG2250 import Alg011
@@ -15,6 +21,9 @@ from alg014_MG2250 import Alg014
 from alg015_MG2250 import Alg015
 from alg016_MG2250 import Alg016
 from alg017_MG2250 import Alg017
+from alg018_MG2250 import Alg018
+from alg019_MG2250 import Alg019
+from alg021_MG2250 import Alg021
 
 
 def main():
@@ -34,6 +43,18 @@ def main():
                 alg = Alg004(graph)
             elif algcode == 'alg004_s2_MG2250':  # 伺服阀稳态过程开口度s2
                 alg = Alg004_S2(graph)
+            elif algcode == 'alg004_s3_MG2250':
+                alg = Alg004_S3
+            elif algcode == 'alg004_s4_MG2250':
+                alg = Alg004_S4
+            elif algcode == 'alg004_s5_MG2250':
+                alg = Alg004_S5
+            elif algcode == 'alg004_s6_MG2250':
+                alg = Alg004_S6
+            elif algcode == 'alg004_s7_MG2250':
+                alg = Alg004_S7
+            elif algcode == 'alg004_s8_MG2250':
+                alg = Alg004_S8
             elif algcode == 'alg005_MG2250':
                 alg = Alg005(graph)
             elif algcode == 'alg010_MG2250':
@@ -48,6 +69,12 @@ def main():
                 alg = Alg016(graph)
             elif algcode == 'alg017_MG2250':
                 alg = Alg017(graph)
+            elif algcode == 'alg018_MG2250':
+                alg = Alg018(graph)
+            elif algcode == 'alg019_MG2250':
+                alg = Alg019(graph)
+            elif algcode == 'alg021_MG2250':
+                alg = Alg021(graph)
             else:
                 raise Exception("Algorithm not registered !")
 
