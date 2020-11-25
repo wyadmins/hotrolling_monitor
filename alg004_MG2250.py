7e57288e5f03a06da16a16ff161cba36a5376766
LAPTOP-GDVL414K\wyadmins <yong.wang@ronds.com.cn>
2020-11-25 23:27:09 +0800

    [update]: alg004增加配置参数；修改切头规则；

--- alg004_MG2250.py
+++ alg004_MG2250.py
@@ -3,15 +3,16 @@ Provides:
 伺服阀泄漏指标，计算稳态伺服阀开口度
 ==============
 Input Signals (4):
-* 实际辊缝值：gap_act
-* 速度给定值: speed_ref
-* 伺服阀开口度：sv_out
+* 实际辊缝值：    gap_act
+* 速度给定值:     speed_ref
+* 伺服阀开口度：  sv_out
 * 伺服阀前截止阀: cutoff_valve
 
-Parameter Configs (3)：
+Parameter Configs (4)：
 * 实际辊缝给定下限
 * 实际辊缝给定上限
 * 稳态持续时间下限(秒)
+* 标志值
 ==============
 Outputs:
 指标   |  指标id
@@ -20,6 +21,11 @@ Outputs:
 * 伺服阀开口度标准差：10401
 * 伺服阀开口度最大值：10402
 * 伺服阀开口度最小值：10403
+==============
+Notes：
+标志值对于助卷辊为停机信号，对于卷曲夹送辊为位置给定值
+==============
+Author: wangyong
 """
 
 import numpy as np
@@ -40,17 +46,17 @@ class Alg004:
         measdate = []
         if not df.empty:
             idx = (df.gap_act >= algparas[0]) & (df.gap_act <= algparas[1]) \
-                  & (df.speed_ref == 0) & (df.cutoff_valve == 1)   # 停机伺服阀稳态开口度
+                  & (df.speed_ref == algparas[3]) & (df.cutoff_valve == 1)   # 停机伺服阀稳态开口度
             n = algparas[2] * df.num_per_sec
             re_iter = com_util.Reg.finditer(idx, n)
             for i in re_iter:
                 [stidx, edidx] = i.span()
                 if edidx - stidx > n:
-                    measdate.append(df.index[stidx + n])
-                    avg_sv_out.append(np.mean(df.sv_out[stidx+n:edidx]))
-                    std_sv_out.append(np.std(df.sv_out[stidx+n:edidx]))
-                    max_sv_out.append(np.max(df.sv_out[stidx+n:edidx]))
-                    min_sv_out.append(np.min(df.sv_out[stidx+n:edidx]))
+                    measdate.append(df.index[stidx + int(n/5)])
+                    avg_sv_out.append(np.mean(df.sv_out[stidx+int(n/5):edidx]))
+                    std_sv_out.append(np.std(df.sv_out[stidx+int(n/5):edidx]))
+                    max_sv_out.append(np.max(df.sv_out[stidx+int(n/5):edidx]))
+                    min_sv_out.append(np.min(df.sv_out[stidx+int(n/5):edidx]))
         return measdate, avg_sv_out, std_sv_out, max_sv_out, min_sv_out
 
     def execute(self):
